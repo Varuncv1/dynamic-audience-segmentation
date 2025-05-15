ea5e5047ee52447cbd1cc4d0e5c96aaf9650c397
CV VARUN <varuncv2001@gmail.com>
2025-05-14 20:44:29 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 # e.g. in prototyping you hard-coded batch sizes or filepaths
 STREAM_BATCH_SIZE = 20
-STREAM_SLEEP_SEC   = 1
+STREAM_SLEEP_SEC   = 0
 N_CLUSTERS         = 5
 
 # point to your processed sessions CSV
